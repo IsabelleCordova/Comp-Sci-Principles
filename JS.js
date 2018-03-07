3ec24b7d3a697cd327f27425a58322397db4dc33
Isabelle <isabelleloveschocolate@gmail.com>
2018-03-07 10:06:53 -0800

    Test page added

---
+++ JS.js
@@ -0,0 +1,21 @@
+$(document).ready(function(){
+alert("script.js is working!");
+});
+
+$(document)
+  .mousemove(function(e) {
+    $('.cursor')
+      .eq(0)
+      .css({
+        left: e.pageX,
+        top: e.pageY
+      });
+    setTimeout(function() {
+      $('.cursor')
+        .eq(1)
+        .css({
+          left: e.pageX,
+          top: e.pageY
+        });
+    }, 100);
+  })
